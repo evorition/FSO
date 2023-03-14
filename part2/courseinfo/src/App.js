3baf24179aeae7af3967814a1e000515f684b181
Maxim Makovskiy <makovskiyms@gmail.com>
2023-03-14 19:00:10 +0600

    Complete exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,28 +1,4 @@
-const Header = ({ text }) => <h1>{text}</h1>;
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, part) => total + part.exercises, 0);
-
-  return <strong>total of {sum} exercises</strong>;
-};
-
-const Course = (props) => {
-  return (
-    <>
-      <Header text={props.course.name} />
-      {props.course.parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-      <Total parts={props.course.parts} />
-    </>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
