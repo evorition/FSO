239d6ec9980132f81e2b89bef0e8ed04c4c6ee14
Maxim Makovskiy <makovskiyms@gmail.com>
2023-03-15 13:51:34 +0600

    Complete exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
+
+  const filteredPersons =
+    filterText === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filterText)
+        );
+
+  const handleFilterChange = (event) => {
+    const newFilterText = event.target.value.toLowerCase();
+    setFilterText(newFilterText);
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -20,6 +36,7 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
     if (persons.findIndex((persona) => persona.name === newName) !== -1) {
@@ -37,6 +54,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={filterText} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new </h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,8 +71,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
+      {filteredPersons.map((person) => (
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
