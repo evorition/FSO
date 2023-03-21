a784be8d49199eb31961ab168c3cd88bdb03e2a5
Maxim Makovskiy <makovskiyms@gmail.com>
2023-03-21 17:26:13 +0600

    Complete exercise 4.5

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -6,7 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikedBlog = null;
+  let mostLikes = 0;
+
+  for (const blog of blogs) {
+    if (blog.likes >= mostLikes) {
+      mostLikes = blog.likes;
+      mostLikedBlog = blog;
+    }
+  }
+
+  return mostLikedBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
