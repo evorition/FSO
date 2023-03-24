ca5b89c64cd0bcae148f0ba9ad1c79b019ed6b7a
Maxim Makovskiy <makovskiyms@gmail.com>
2023-03-24 13:36:17 +0600

    Complete exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs/", blogsRouter);
+app.use("/api/users/", usersRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,7 +2,7 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (request, response) => {
-  blogs = await Blog.find({});
+  const blogs = await Blog.find({});
 
   response.json(blogs);
 });
