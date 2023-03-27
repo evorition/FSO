ab9ebf4ea8496a97aa61645f7f58737f12a3d148
Maxim Makovskiy <makovskiyms@gmail.com>
2023-03-27 17:47:23 +0600

    Complete exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [expand, setExpand] = useState(false);
+
+  const showWhenExpanded = { display: expand ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setExpand(!expand)}>
+          {expand ? "hide" : "show"}
+        </button>
+      </div>
+      <div style={showWhenExpanded}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
