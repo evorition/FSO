195ebc49c05adcd7425a3f25789a017ae9c7dbbf
Maxim Makovskiy <makovskiyms@gmail.com>
2023-04-24 16:59:08 +0600

    Complete exercise 9.22

--- part9/patientor/server/src/types.ts
+++ part9/patientor/server/src/types.ts
@@ -4,13 +4,53 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
-
 export interface DiagnoseEntry {
   code: string;
   name: string;
   latin?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
 
 export interface PatientEntry {
   id: string;
